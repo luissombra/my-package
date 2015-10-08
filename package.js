c056ea6bb2ef4f9a421d61f671ff6b1f743c530e
Luis <luisinweb@gmail.com>
2015-10-08 08:42:16 -0300

    atualizado para a versão 2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'netrics:my-package',
-  version: '1.2.0',
+  version: '2.0.0',
   // Brief, one-line summary of the package.
   summary: 'pacote de teste',
   // URL to the Git repository containing the source code for this package.
