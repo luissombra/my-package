e44be986fbe768aae538157bb57e34598682b063
Luis <luisinweb@gmail.com>
2015-10-08 09:24:56 -0300

    correção no número da versão

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'netrics:my-package',
-  version: 'v2.1.0',
+  version: '2.1.0',
   // Brief, one-line summary of the package.
   summary: 'pacote de teste',
   // URL to the Git repository containing the source code for this package.
